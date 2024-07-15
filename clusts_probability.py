10c9a004683d4bc017ffce1018b40db09d5986e7
Dragonborn <78375175+MLDovakin@users.noreply.github.com>
2024-07-15 17:57:40 +0300

    Update clusts_probability.py

--- clusts_probability.py
+++ clusts_probability.py
@@ -1,4 +1,4 @@
-
+from scipy.spatial.distance import cdist
 
 def get_prob_summ(target_emb, cl_d, alpha):
 
@@ -6,9 +6,9 @@ def get_prob_summ(target_emb, cl_d, alpha):
 
   for cl, emb in cl_d.items():
 
-    centroid_emb = np.mean(emb, axis=0)
+    centroid_emb = np.mean(emb, axis=0) 
 
-    diff_e = np.linalg.norm(target_emb - centroid_emb)
+    diff_e = cdist([target_emb], [centroid_emb], metric='euclidean')[0]
 
     closes_summ.append( ((1 + diff_e**2)/alpha)**(-((alpha+1)/2) ) )
 
@@ -30,11 +30,12 @@ def get_probs(target_emb, clust_emb, cl_d, alpha):
 
 sampling_clusts = {}
 alpha = 1
+n = 0
+for data_point in range(len(df)) :
 
-for data_point in range(len(df)):
-  
   data_point_name = df['Cluster'][data_point]
   point_embedding  = df['Embeddings'][data_point]
+
 
   prob_list = []
   point_id  = 0
@@ -44,7 +45,9 @@ for data_point in range(len(df)):
     point_id  += 1
 
     prob_summ = get_probs(point_embedding, prob_emb, cl_d, alpha)
-    prob_list.append((prob_clust, prob_summ))
-    print(data_point_name, prob_clust, prob_summ)
+    prob_list.append((prob_clust, prob_summ.tolist()[0]))
 
-  sampling_clusts.update({str(point_id)+ '_' + str(data_point_name): prob_list })
+    #print(data_point_name, point_id, prob_clust, prob_summ)
+  #print({str(data_point)+ '_' + str(data_point_name): prob_list })
+  #n +=1
+  sampling_clusts.update({str(point_id) + '_df_ind_'+ str(data_point)+ '_clust_' + str(data_point_name): prob_list })
